1b7400ce80b307eb45f8d91102e71cd8963183ae
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-07-01 18:01:36 +0530

    new

---
+++ if_else12.c
@@ -0,0 +1,14 @@
+ income == 350000
+
+ if(income<30000 )
+ final_income= income -  ((income*5)/100)
+
+
+ unit = 50
+
+ if(unit<100)
+ {
+    price = unit * 1
+    final_price = price + price*5 /100
+
+ }
