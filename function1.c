4c11be26b82d034172ba91590582c6b4ae391c2a
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-07-22 18:03:48 +0530

    function

---
+++ function1.c
@@ -0,0 +1,25 @@
+#include<stdio.h> 
+void printline()
+{
+    printf("----------------------------------------------\n");
+}
+
+void main()
+{
+    printf("name : sahil \n");
+    printline();
+    printf("age : 18 \n");
+    printline();
+  
+    printf("name : parth \n");
+  
+    printline();
+    printf("age : 20\n");
+    printline();
+
+    printf("name : janard \n");
+    printline();
+
+    printf("age : 21 \n");
+    printline();
+}
---
+++ function2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void printsymbol(char sy,int n)
+{
+    int i;
+    for (i = 1; i <= n;i++)
+    {
+        printf("%c", sy);
+    }
+
+    printf("\n");
+}
+void main()
+{
+    char symbol;
+    int num;
+
+    printf("Enter symbol ");
+
+    scanf("%c", &symbol);
+
+    printf("Enter number ");
+
+    scanf("%d",&num);
+
+    printf("name : sahil \n");
+
+    printsymbol(symbol,num);
+
+    printf("age : 18 \n");
+
+    printsymbol(symbol,num);
+
+    printf("name : parth \n");
+
+    printsymbol(symbol,num);
+
+    printf("age : 20\n");
+
+    printsymbol(symbol,num);
+
+    printf("name : janard \n");
+
+    printsymbol(symbol,num);
+
+    printf("age : 21 \n");
+
+    printsymbol(symbol,num);
+}
